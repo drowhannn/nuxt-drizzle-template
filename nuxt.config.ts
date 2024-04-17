d02f97dc5c4754cf62ac44fc305794207574f564
Rohan Dhimal <drowhann@gmail.com>
2024-04-17 08:28:06 +0545

    feat: add eslint config with husky and lint-staged

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@nuxt/eslint'],
   devtools: { enabled: true },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
